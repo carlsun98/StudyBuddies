0c42e30764e73838170b142733fc0e3ed1103ae1
Malika <moak@princeton.edu>
2018-04-15 21:57:14 -0400

    made pages

---
+++ src/iOS/StudyBuddy/StudyBuddy/ClassListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ClassListViewController.swift
+//  StudyBuddy
+//
+//  Created by Malika Oak on 4/15/18.
+//  Copyright © 2018 StudyBuddies LLC. All rights reserved.
+//
+
+import UIKit
+
+class ClassListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
